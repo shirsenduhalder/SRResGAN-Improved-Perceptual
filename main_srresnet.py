7c972458e42ba7d2ef246b8ec19eca82d2c60eca
shirsenduhalder <haldershirsendu1@gmail.com>
2019-07-05 19:59:17 +0530

    First commit - added via upload

---
+++ main_srresnet.py
@@ -0,0 +1,229 @@
+import argparse, os
+import torch
+import math, random
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+import torchvision.utils as utils
+from srresnet import _NetG, _NetD
+from dataset import DatasetFromHdf5
+from torchvision import models
+import torch.utils.model_zoo as model_zoo
+from loss import *
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorboardX import SummaryWriter
+
+STEPS = 0
+
+# Training settings
+parser = argparse.ArgumentParser(description="PyTorch SRResNet")
+parser.add_argument("--batchSize", type=int, default=16, help="training batch size")
+parser.add_argument("--nEpochs", type=int, default=500, help="number of epochs to train for")
+parser.add_argument("--lr", type=float, default=1e-4, help="Learning Rate. Default=1e-4")
+parser.add_argument("--step", type=int, default=200, help="Sets the learning rate to the initial LR decayed by momentum every n epochs, Default: n=500")
+parser.add_argument("--cuda", action="store_true", help="Use cuda?")
+parser.add_argument("--resume", default="", type=str, help="Path to checkpoint (default: none)")
+parser.add_argument("--start-epoch", default=1, type=int, help="Manual epoch number (useful on restarts)")
+parser.add_argument("--threads", type=int, default=0, help="Number of threads for data loader to use, Default: 1")
+parser.add_argument("--pretrained", default="", type=str, help="path to pretrained model (default: none)")
+parser.add_argument("--vgg_loss", action="store_true", help="Use content loss?")
+parser.add_argument("--gpus", default="0", type=str, help="gpu ids (default: 0)")
+#changed
+parser.add_argument("--dis_perceptual_loss", action="store_true", help="Use perceptual loss from discriminator?")
+parser.add_argument("--gen_adversarial_loss", action="store_true", help="Use adversarial loss of generator?")
+parser.add_argument("--coverage", action="store_true", help="Use coverage?")
+parser.add_argument("--sample_dir", default="samples/", help="Path to save traiing samples")
+
+def main():
+
+    #changed
+    # global opt, model, netContent
+
+    global opt, model_G, model_D, netContent, writer
+
+    writer = SummaryWriter(logdir="logs/", comment="-srgan-")
+    opt = parser.parse_args()
+    print(opt)
+
+    cuda = opt.cuda
+    if cuda:
+        print("=> use gpu id: '{}'".format(opt.gpus))
+        os.environ["CUDA_VISIBLE_DEVICES"] = opt.gpus
+        if not torch.cuda.is_available():
+                raise Exception("No GPU found or Wrong gpu id, please run without --cuda")
+
+    opt.seed = random.randint(1, 10000)
+    print("Random Seed: ", opt.seed)
+    torch.manual_seed(opt.seed)
+    if cuda:
+        torch.cuda.manual_seed(opt.seed)
+
+    cudnn.benchmark = True
+
+    print("===> Loading datasets")
+    train_set = DatasetFromHdf5("data/srresnet_x4.h5")
+    training_data_loader = DataLoader(dataset=train_set, num_workers=opt.threads, \
+        batch_size=opt.batchSize, shuffle=True)
+
+    print('===> Loading VGG model')
+    netVGG = models.vgg19()
+    netVGG.load_state_dict(model_zoo.load_url('https://download.pytorch.org/models/vgg19-dcbb9e9d.pth'))
+    class _content_model(nn.Module):
+        def __init__(self):
+            super(_content_model, self).__init__()
+            self.feature = nn.Sequential(*list(netVGG.features.children())[:-1])
+            
+        def forward(self, x):
+            out = self.feature(x)
+            return out
+
+    netContent = _content_model()
+
+    print("===> Building model")
+    # changed
+    # Building generator and discriminator
+    model_G = _NetG()
+    
+    #changed
+    model_D = _NetD()
+    criterion_G = GeneratorLoss(netContent, model_D)
+    criterion_D = nn.BCELoss()
+
+    print("===> Setting GPU")
+    if cuda:
+        #changed
+        model_G = model_G.cuda()
+        model_D = model_D.cuda()
+        criterion_G = criterion_G.cuda()
+        criterion_D = criterion_D.cuda()
+        if opt.vgg_loss:
+            netContent = netContent.cuda() 
+
+    # optionally resume from a checkpoint
+    if opt.resume:
+        if os.path.isfile(opt.resume):
+            print("=> loading checkpoint '{}'".format(opt.resume))
+            checkpoint = torch.load(opt.resume)
+            opt.start_epoch = checkpoint["epoch"] + 1
+            # changed
+            model_G.load_state_dict(checkpoint["model"].state_dict())
+        else:
+            print("=> no checkpoint found at '{}'".format(opt.resume))
+
+    # optionally copy weights from a checkpoint
+    if opt.pretrained:
+        if os.path.isfile(opt.pretrained):
+            print("=> loading model '{}'".format(opt.pretrained))
+            weights = torch.load(opt.pretrained)
+            # changed
+            model_G.load_state_dict(weights['model'].state_dict())
+        else:
+            print("=> no model found at '{}'".format(opt.pretrained))
+
+    print("===> Setting Optimizer")
+    # changed
+    optimizer_G = optim.Adam(model_G.parameters(), lr=opt.lr)
+    optimizer_D = optim.Adam(model_D.parameters(), lr=opt.lr)
+
+    print("===> Training")
+    for epoch in range(opt.start_epoch, opt.nEpochs + 1):
+        # changed
+        train(training_data_loader, optimizer_G, optimizer_D, model_G, model_D, criterion_G, criterion_D, epoch)
+        save_checkpoint(model_G, epoch)
+
+def adjust_learning_rate(optimizer, epoch):
+    """Sets the learning rate to the initial LR decayed by 10"""
+    lr = opt.lr * (0.1 ** (epoch // opt.step))
+    return lr 
+
+def train(training_data_loader, optimizer_G, optimizer_D, model_G, model_D, criterion_G, criterion_D, epoch):
+
+    lr = adjust_learning_rate(optimizer_G, epoch-1)
+    
+    for param_group in optimizer_G.param_groups:
+        param_group["lr"] = lr
+    
+    for param_group in optimizer_D.param_groups:
+        param_group["lr"] = lr
+
+    print("Epoch={}, lr={}".format(epoch, optimizer_G.param_groups[0]["lr"]))
+    model_G.train()
+    model_D.train()
+    global STEPS
+
+    for iteration, batch in enumerate(training_data_loader, 1):
+
+        input, target = Variable(batch[0]), Variable(batch[1], requires_grad=False)
+        
+
+        STEPS += input.shape[0]
+
+        if opt.cuda:
+            input = input.cuda()
+            target = target.cuda()
+
+        output = model_G(input)
+        
+        target_real = Variable(torch.rand(opt.batchSize)*0.5 + 0.7).cuda()
+        target_fake = Variable(torch.rand(opt.batchSize)*0.3).cuda()
+
+        model_D.zero_grad()
+        real_out = model_D(target)[-1]
+        fake_out = model_D(output)[-1]
+        loss_d = criterion_D(real_out, target_real) + criterion_D(fake_out, target_fake)
+        loss_d.backward(retain_graph=True)
+        optimizer_D.step()
+
+
+        # if opt.vgg_loss:
+        #     content_input = netContent(output)
+        #     content_target = netContent(target)
+        #     content_target = content_target.detach()
+        #     content_loss = criterion(content_input, content_target)
+
+        optimizer_G.zero_grad()
+        loss_g = criterion_G(opt.gen_adversarial_loss, opt.vgg_loss, opt.dis_perceptual_loss, opt.coverage, fake_out, output, target, opt)
+
+        # if opt.vgg_loss:
+        #     netContent.zero_grad()
+        #     content_loss.backward(retain_graph=True)
+
+        loss_g.backward()
+
+        optimizer_G.step()
+
+        writer.add_scalar("Loss_G", loss_g.item(), STEPS)
+        writer.add_scalar("Loss_D", loss_d.item(), STEPS)
+
+        if iteration%1000 == 0:
+            # if opt.vgg_loss:
+            #     print("===> Epoch[{}]({}/{}): Loss: {:.5} Content_loss {:.5}".format(epoch, iteration, len(training_data_loader), loss.data[0], content_loss.data[0]))
+            # else:
+            
+            sample_img = utils.make_grid(torch.cat([output.detach().clone(), target], dim=0), padding=2, normalize=True)
+            if not os.path.exists(opt.sample_dir):
+                os.makedirs(opt.sample_dir)
+            
+            utils.save_image(sample_img, os.path.join(opt.sample_dir, "Epoch-{}--Iteration-{}.png".format(epoch, iteration)), padding=5)
+
+        if iteration % 100 == 0:
+            print("===> Epoch[{}]({}/{}): G_Loss: {:.3}, D_Loss: {:.3} ".format(epoch, iteration, len(training_data_loader), loss_g.item(), loss_d.item()))
+
+
+
+
+def save_checkpoint(model, epoch):
+    model_out_path = "checkpoint/" + "model_epoch_{}.pth".format(epoch)
+    state = {"epoch": epoch ,"model": model}
+    if not os.path.exists("checkpoint/"):
+        os.makedirs("checkpoint/")
+
+    torch.save(state, model_out_path)
+
+    print("Checkpoint saved to {}".format(model_out_path))
+
+if __name__ == "__main__":
+    main()
---
+++ srresnet.py
@@ -0,0 +1,162 @@
+import torch
+import torch.nn as nn
+import math
+
+class _Residual_Block(nn.Module):
+    def __init__(self):
+        super(_Residual_Block, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.in1 = nn.InstanceNorm2d(64, affine=True)
+        self.relu = nn.LeakyReLU(0.2, inplace=True)
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.in2 = nn.InstanceNorm2d(64, affine=True)
+
+    def forward(self, x):
+        identity_data = x
+        output = self.relu(self.in1(self.conv1(x)))
+        output = self.in2(self.conv2(output))
+        output = torch.add(output,identity_data)
+        return output 
+
+class _NetG(nn.Module):
+    def __init__(self):
+        super(_NetG, self).__init__()
+
+        self.conv_input = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=9, stride=1, padding=4, bias=False)
+        self.relu = nn.LeakyReLU(0.2, inplace=True)
+        
+        self.residual = self.make_layer(_Residual_Block, 16)
+
+        self.conv_mid = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn_mid = nn.InstanceNorm2d(64, affine=True)
+
+        self.upscale4x = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.PixelShuffle(2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(in_channels=64, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.PixelShuffle(2),
+            nn.LeakyReLU(0.2, inplace=True),
+        )
+
+        self.conv_output = nn.Conv2d(in_channels=64, out_channels=3, kernel_size=9, stride=1, padding=4, bias=False)
+        
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                if m.bias is not None:
+                    m.bias.data.zero_()
+
+    def make_layer(self, block, num_of_layer):
+        layers = []
+        for _ in range(num_of_layer):
+            layers.append(block())
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.relu(self.conv_input(x))
+        residual = out
+        out = self.residual(out)
+        out = self.bn_mid(self.conv_mid(out))
+        out = torch.add(out,residual)
+        out = self.upscale4x(out)
+        out = self.conv_output(out)
+        return out
+
+class _NetD(nn.Module):
+    def __init__(self):
+        super(_NetD, self).__init__()
+
+        # input is (3) x 96 x 96
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.relu1 = nn.LeakyReLU(0.2, inplace=True)
+
+        # state size. (64) x 96 x 96
+        self.conv2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, stride=2, padding=1, bias=False)            
+        self.batchnorm2 = nn.BatchNorm2d(64)
+        self.relu2 = nn.LeakyReLU(0.2, inplace=True)
+
+        # state size. (64) x 96 x 96
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1, bias=False)
+        self.batchnorm3 = nn.BatchNorm2d(128)
+        self.relu3 = nn.LeakyReLU(0.2, inplace=True)
+        
+        # state size. (64) x 48 x 48
+        self.conv4 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=4, stride=2, padding=1, bias=False)
+        self.batchnorm4 = nn.BatchNorm2d(128)
+        self.relu4 = nn.LeakyReLU(0.2, inplace=True)
+
+        # state size. (128) x 48 x 48
+        self.conv5 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1, bias=False)
+        self.batchnorm5 = nn.BatchNorm2d(256)
+        self.relu5 = nn.LeakyReLU(0.2, inplace=True)
+
+        # state size. (256) x 24 x 24
+        self.conv6 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=4, stride=2, padding=1, bias=False)
+        self.batchnorm6 = nn.BatchNorm2d(256)
+        self.relu6 = nn.LeakyReLU(0.2, inplace=True)
+
+        # state size. (256) x 12 x 12
+        self.conv7 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, stride=1, padding=1, bias=False)
+        self.batchnorm7 = nn.BatchNorm2d(512)
+        self.relu7 = nn.LeakyReLU(0.2, inplace=True)
+
+        # state size. (512) x 12 x 12
+        self.conv8 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=4, stride=2, padding=1, bias=False)
+        self.batchnorm8 = nn.BatchNorm2d(512)
+        self.relu8 = nn.LeakyReLU(0.2, inplace=True)
+
+        self.relu9 = nn.LeakyReLU(0.2, inplace=True)
+        self.fc1 = nn.Linear(512 * 6 * 6, 1024)
+        self.fc2 = nn.Linear(1024, 1)
+        self.sigmoid = nn.Sigmoid()
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                m.weight.data.normal_(0.0, 0.02)
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.normal_(1.0, 0.02)
+                m.bias.data.fill_(0)
+
+    def forward(self, input):
+
+        out1 = self.relu1(self.conv1(input))
+
+        out2 = self.relu2(self.batchnorm2(self.conv2(out1)))
+
+        out3 = self.relu3(self.batchnorm3(self.conv3(out2)))
+
+        out4 = self.relu4(self.batchnorm4(self.conv4(out3)))
+
+        out5 = self.relu5(self.batchnorm5(self.conv5(out4)))
+
+        out6 = self.relu6(self.batchnorm6(self.conv6(out5)))
+
+        out7 = self.relu7(self.batchnorm7(self.conv7(out6)))
+
+        out8 = self.relu8(self.batchnorm8(self.conv8(out7)))
+        
+        out1 = out1.view(out1.size(0), -1)
+        out2 = out2.view(out1.size(0), -1)
+        out3 = out3.view(out1.size(0), -1)
+        out4 = out4.view(out1.size(0), -1)
+        out5 = out5.view(out1.size(0), -1)
+        out6 = out6.view(out1.size(0), -1)
+        out7 = out7.view(out1.size(0), -1)
+
+        # state size. (512) x 6 x 6
+        out8 = out8.view(out1.size(0), -1)
+
+        # state size. (512 x 6 x 6)
+        out_fc1 = self.fc1(out8)
+
+        # state size. (1024)
+        out_fc1 = self.relu9(out_fc1)
+
+        out_fc2 = self.fc2(out_fc1)
+        out_final = self.sigmoid(out_fc2)
+        out_final = out_final.view(-1, 1).squeeze(1)
+        
+        return out1, out2, out3, out4, out5, out6, out7, out_final

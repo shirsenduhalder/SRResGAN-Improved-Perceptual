e52a4b967fe196dfc251f71eca6cb648f2614c09
Akella17 <ravitej.akella@gmail.com>
2019-07-09 03:15:07 +0530

    Added Huber loss and p_loss coefficient

--- main_srresnet.py
+++ main_srresnet.py
@@ -30,16 +30,20 @@ parser.add_argument("--resume", default="", type=str, help="Path to checkpoint (
 parser.add_argument("--start-epoch", default=1, type=int, help="Manual epoch number (useful on restarts)")
 parser.add_argument("--threads", type=int, default=0, help="Number of threads for data loader to use, Default: 1")
 parser.add_argument("--pretrained", default="", type=str, help="path to pretrained model (default: none)")
-parser.add_argument("--vgg_loss", action="store_true", help="Use content loss?")
 parser.add_argument("--gpus", default="0", type=str, help="gpu ids (default: 0)")
-#changed
-parser.add_argument("--dis_perceptual_loss", action="store_true", help="Use perceptual loss from discriminator?")
-parser.add_argument("--adversarial_loss", action="store_true", help="Use adversarial loss of generator?")
-parser.add_argument("--coverage", action="store_true", help="Use coverage?")
-parser.add_argument("--softmax_loss", action="store_true", help="Use softmax normalized loss for discriminator perceptual loss?")
 parser.add_argument("--sample_dir", default="outputs/samples/", help="Path to save traiing samples")
 parser.add_argument("--logs_dir", default="outputs/logs/", help="Path to save logs")
 parser.add_argument("--checkpoint_dir", default="outputs/checkpoint/", help="Path to save checkpoint")
+parser.add_argument("--adversarial_loss_coefficient", default=0.001, help="Coefficient for adversarial loss")
+parser.add_argument("--vgg_loss_coefficient", default=0.006, help="Coefficient for VGG loss")
+#changed
+parser.add_argument("--vgg_loss", action="store_true", help="Use content loss?")
+parser.add_argument("--adversarial_loss", action="store_true", help="Use adversarial loss of generator?")
+parser.add_argument("--dis_perceptual_loss", action="store_true", help="Use perceptual loss from discriminator?")
+parser.add_argument("--huber_loss", action="store_true", help="Uses huber loss for computing perceptual loss from discriminator?")
+parser.add_argument("--softmax_loss", action="store_true", help="Use softmax normalized loss for discriminator perceptual loss?")
+parser.add_argument("--coverage", action="store_true", help="Use coverage?")
+parser.add_argument("--dis_perceptual_loss_coefficient", default=0.001, help="Coefficient for perceptual loss from discriminator")
 
 def main():
 
@@ -58,7 +62,7 @@ def main():
         assert opt.dis_perceptual_loss, "Softmax normalization is only valid for Discriminator Perceptual loss"
 
 
-    out_folder = "Softmax({})_PerLoss({})_GANloss({})_VGGloss({})_coverage({})".format(opt.softmax_loss, opt.dis_perceptual_loss, opt.adversarial_loss, opt.vgg_loss, opt.coverage)
+    out_folder = "Softmax({})_PerLoss({})_GANloss({})_VGGloss({})_coverage({})_huber({})_perCoeff({})".format(opt.softmax_loss, opt.dis_perceptual_loss, opt.adversarial_loss, opt.vgg_loss, opt.coverage, opt.huber_loss, opt.dis_perceptual_loss_coefficient)
 
     writer = SummaryWriter(logdir = os.path.join(opt.logs_dir, out_folder), comment="-srgan-")
 

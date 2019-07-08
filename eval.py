2f8dee8c269d5bd6b3d6a87983cf3d75ac2701a7
Akella17 <ravitej.akella@gmail.com>
2019-07-08 16:02:13 +0530

    Removed MATLAB dependency in eval.py

--- eval.py
+++ eval.py
@@ -1,18 +1,40 @@
-import matlab.engine
+#import matlab.engine
 import argparse, os
 import torch
 from torch.autograd import Variable
 import numpy as np
+from numpy import *
 import time, math, glob
 import scipy.io as sio
 import cv2
+from math import log10,floor 
 
 parser = argparse.ArgumentParser(description="PyTorch SRResNet Eval")
 parser.add_argument("--cuda", action="store_true", help="use cuda?")
-parser.add_argument("--model", default="model/model_srresnet.pth", type=str, help="model path")
+parser.add_argument("--model", default="checkpoint/model_srresnet.pth", type=str, help="model path")
 parser.add_argument("--dataset", default="Set5", type=str, help="dataset name, Default: Set5")
 parser.add_argument("--scale", default=4, type=int, help="scale factor, Default: 4")
 parser.add_argument("--gpus", default="0", type=str, help="gpu ids (default: 0)")
+
+
+def rgb2ycbcr(img, only_y=True):
+    '''same as matlab rgb2ycbcr
+    '''
+    in_img_type = img.dtype
+    img.astype(np.float64)
+    if in_img_type != np.uint8:
+        img *= 255.
+    # convert
+    if only_y:
+        rlt = np.dot(img, [65.481, 128.553, 24.966]) / 255.0 + 16.0
+    else:
+        rlt = np.matmul(img, [[65.481, -37.797, 112.0], [128.553, -74.203, -93.786],
+                                [24.966, 112.0, -18.214]]) / 255.0 + [16, 128, 128]
+    if in_img_type == np.uint8:
+        rlt = rlt.round()
+    else:
+        rlt /= 255.
+    return rlt.astype(in_img_type)
 
 def PSNR(pred, gt, shave_border=0):
     height, width = pred.shape[:2]
@@ -26,7 +48,7 @@ def PSNR(pred, gt, shave_border=0):
 
 opt = parser.parse_args()
 cuda = opt.cuda
-eng = matlab.engine.start_matlab()
+#eng = matlab.engine.start_matlab()
 
 if cuda:
     print("=> use gpu id: '{}'".format(opt.gpus))
@@ -75,12 +97,18 @@ for image_name in image_list:
     im_h = HR_4x.data[0].numpy().astype(np.float32)
 
     im_h = im_h*255.
-    im_h = np.clip(im_h, 0., 255.)
+    im_h = np.clip(im_h, 0., 255.)  
     im_h = im_h.transpose(1,2,0).astype(np.float32)
 
-    im_h_matlab = matlab.double((im_h / 255.).tolist())
-    im_h_ycbcr = eng.rgb2ycbcr(im_h_matlab)
-    im_h_ycbcr = np.array(im_h_ycbcr._data).reshape(im_h_ycbcr.size, order='F').astype(np.float32) * 255.
+    #im_h_matlab = matlab.double((im_h / 255.).tolist())
+    im_h_matlab = double(im_h / 255.)
+    # print(im_h_matlab)
+    # im_h  = convert(im_h_matlab)
+    # print(im_h)
+    #im_h_ycbcr = eng.rgb2ycbcr(im_h_matlab)
+    im_h_ycbcr = rgb2ycbcr(im_h_matlab, only_y=False)*255
+    # im_h_ycbcr = np.array(im_h_ycbcr).reshape(im_h_ycbcr.size, order='F').astype(np.float32) * 255.
+    # print(im_h_ycbcr.shape)
     im_h_y = im_h_ycbcr[:,:,0]
 
     psnr_predicted = PSNR(im_gt_y, im_h_y,shave_border=opt.scale)
